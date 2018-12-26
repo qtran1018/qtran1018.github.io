31488c2691cf2209a5b72f0280a98dcd2898f1bb
Quang Tran <qtran1018@gmail.com>
2018-12-26 03:22:09 -0500

    big update
    Column updates
    Added Logo

--- knights-chronicle/make-table.js
+++ knights-chronicle/make-table.js
@@ -13,10 +13,10 @@ function loadXMLDoc() {
 function makeTable(xml) {
     let i;
     let xmlDoc = xml.responseXML;
-    let table="<tr><th>Name</th><th>Type</th><th>Role</th><th>Element</th></tr>";
+    let heroTable="<tr><th>Name</th><th>Type</th><th>Role</th><th>Element</th></tr>";
     let x = xmlDoc.getElementsByTagName("HERO");
     for (i = 0; i <x.length; i++) {
-        table += "<tr><td>" +
+        heroTable += "<tr><td>" +
             x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
                 "</td><td>" +
             x[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue +
@@ -26,5 +26,27 @@ function makeTable(xml) {
             x[i].getElementsByTagName("ELEMENT")[0].childNodes[0].nodeValue +
                 "</td></tr>";    
             }
-    document.getElementById("hero-table").innerHTML = table;
+    document.getElementById("hero-table").innerHTML = heroTable;
+}
+
+function filterTable() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("hero-table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+        }
+    }
+  }
 }
